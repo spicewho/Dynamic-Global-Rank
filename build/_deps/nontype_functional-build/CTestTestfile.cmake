e9c93370337de4d4d5d325a79d141d844f2a7d38
Spice <Spice@DESKTOP-0NQAEPH>
2026-07-28 23:49:05 -0400

    Revert "Stop tracking build directory"
    This reverts commit c0abb87eb0488a6a65d91576e30b3fad5b750576.

---
+++ build/_deps/nontype_functional-build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/nontype_functional-src
+# Build directory: C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/nontype_functional-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/_deps/tuliphook-subbuild/tuliphook-populate-prefix/tmp/tuliphook-populate-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file LICENSE.rst or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/tuliphook-src")
+  file(MAKE_DIRECTORY "C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/tuliphook-src")
+endif()
+file(MAKE_DIRECTORY
+  "C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/tuliphook-build"
+  "C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/tuliphook-subbuild/tuliphook-populate-prefix"
+  "C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/tuliphook-subbuild/tuliphook-populate-prefix/tmp"
+  "C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/tuliphook-subbuild/tuliphook-populate-prefix/src/tuliphook-populate-stamp"
+  "C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/tuliphook-subbuild/tuliphook-populate-prefix/src"
+  "C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/tuliphook-subbuild/tuliphook-populate-prefix/src/tuliphook-populate-stamp"
+)
+
+set(configSubDirs Debug)
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/tuliphook-subbuild/tuliphook-populate-prefix/src/tuliphook-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Users/Spice/Documents/GeodeMods/Dynamic Global Rank/build/_deps/tuliphook-subbuild/tuliphook-populate-prefix/src/tuliphook-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
